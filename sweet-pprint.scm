406f434c37d6f5a0535ef3495a1bc42618fcc2ba
David A. Wheeler <dwheeler@dwheeler.com>
2008-01-11 13:50:43 +0000

    Switched sweet-pprint.scm so modern formatting returns list of chars (more flexible)

--- sweet-pprint.scm
+++ sweet-pprint.scm
@@ -23,8 +23,20 @@
 
 ; TODO: Finish this code.
 ; TODO: Add support for "port"
+; TODO: (a . b) not working for some reason - need to debug.
 
-(define (sweet-is-infix-operator? s)
+(define LP #\( )
+(define RP #\) )
+
+(define (infix-chars? lyst)
+  ; Returns true if the list only contains characters used in infix ops
+  (cond
+    ((null? lyst) #t)
+    ((pair? (member (car lyst) '(#\+ #\- #\* #\/ #\= #\< #\> #\& #\|)))
+      (infix-chars? (cdr lyst)))
+    (#t #f)))
+
+(define (modern-is-infix-operator? s)
   (cond
     ((not (symbol? s))  #f)
     ((eq? s 'and)  #t)
@@ -33,14 +45,7 @@
     ((eq? s ':)  #t)
     (#t (infix-chars? (string->list (symbol->string s))))))
 
-(define (infix-chars? lyst)
-  (cond
-    ((null? lyst) #t)
-    ((pair? (member (car lyst) '(#\+ #\- #\* #\/ #\= #\< #\> #\& #\|)))
-      (infix-chars? (cdr lyst)))
-    (#t #f)))
-
-(define (sweet-is-infixable-list lyst)
+(define (modern-is-infixable-list lyst)
   ; Returns true if lyst can be represented as infix list; else false
   (cond
     ((not (pair? lyst)) #f) ; False if not a pair at all.
@@ -48,102 +53,117 @@
     ((not (pair? (cdr lyst))) #f)   ; Must have 2+ function parameters
     ((not (pair? (cddr lyst))) #f)
     ((not (list? lyst)) #f)         ; Must be a proper list.
-    ((sweet-is-infix-operator? (car lyst)) #t)  ; True if infix operator.
+    ((modern-is-infix-operator? (car lyst)) #t)  ; True if infix operator.
     (#t #f))) ; By default, false.
 
-(define (display-list x)
-  ; call "display" on each item in x, if it's a list; else display x.
+(define (list-item-chars x)
+  ; call modern-charlist on each item in x
   (cond
+    ((null? x) '())
     ((pair? x)
-      (display (car x))
-      (display-list (cdr x)))
-    ((eq? x '()))               ; Do nothing for '().
-    (#t (display x))))
+      (append
+        (modern-charlist (car x))
+        (list-item-chars (cdr x))))
+    (#t (modern-charlist x))))
 
-(define (sweet-write-contents separators obj)
-  ; Writes contents of "list" obj, without its surrounding markers.
+(define (modern-contents separators obj)
+  ; Returns contents representing "list" obj, without its surrounding markers.
   ; if it isn't a pair, it must be from a recursion with an improper list.
   ; Separators is a list of displayable separators.
   (cond
-    ((eq? obj '()) ) ; Do nothing.
+    ((null? obj) '() ) ; Do nothing.
     ((pair? obj)
-      (sweet-write (car obj))
-      (if (not (eq? (cdr obj) '()))
-        (begin
-          (display-list separators)
-          (sweet-write-contents separators (cdr obj)))))
-    (#t (display ". ") (sweet-write obj))
-))
+      (append (modern-charlist (car obj))
+        (if (eqv? (cdr obj) '())
+          '()
+          (append
+            separators
+            (modern-contents separators (cdr obj))))))
+    (#t (append '(#\. #\space)) (modern-charlist obj))))
 
 (define function-call-translate #t)
 
-(define (sweet-write obj)
+(define (modern-charlist obj)
+  ; create a list of characters representing obj.
   (cond
-    ((not (pair? obj)) (write obj))
-    ((and (eq? (car obj) 'quote)
-          (eq? (cddr obj) '()))
-      (display "'") (sweet-write (cadr obj)))
+    ((symbol? obj) (string->list (symbol->string obj)))
+    ((null? obj)   (string->list "()"))
+    ((number? obj) (string->list (number->string obj)))
+    ((char? obj)
+      (append '(#\#) '(#\\)
+        (cond
+          ((eqv? obj #\newline) (string->list "newline"))
+          ((eqv? obj #\space)   (string->list "space"))
+          (#t (list obj)))))
+    ; TODO: string
+    ; OLD: ((not (pair? obj)) (write obj))
+    ((and (pair? obj) (eq? (car obj) 'quote) (eq? (cddr obj) '()))
+      (cons #\' (modern-charlist (cadr obj))))
     ; TODO: quasiquote, comma, comma-splicing
-    ((sweet-is-infixable-list obj) ; Infix.
-      (display "{")
-      (sweet-write-contents (list " " (car obj) " ") (cdr obj))
-      (display "}"))
+    ((modern-is-infixable-list obj) ; Infix?
+      (append '(#\{)
+       (modern-contents
+          (append '(#\ ) (string->list (symbol->string (car obj))) '(#\ ))
+                  (cdr obj))
+      '(#\})))
     ((and (symbol? (car obj))
           (pair? (cdr obj))
           (eq? (cddr obj) '())  ; Exactly one parameter.
-          (sweet-is-infixable-list (cadr obj)))
+          (modern-is-infixable-list (cadr obj)))
       ; Function-call, one-parameter-infix: (f (- n 1)) => f{n - 1}.
-      (display (car obj))
-      ; Don't need to display "{" - handled by infix processing.
-      (sweet-write-contents
-        (string-append " " (symbol->string (caadr obj)) " ")
-        (cdr obj)))
+      (append (modern-charlist (car obj))
+        ; Don't need to display "{" - handled by infix processing.
+        (modern-contents
+          (append '(#\ ) (string->list (symbol->string (car obj))) '(#\ ))
+                  (cdr obj))))
     ((and function-call-translate (symbol? (car obj))
           (or (pair? (cdr obj)) (eq? (cdr obj) '())))
       ; Function call.
       ; Require that cdr is pair or empty list; we don't want
       ; (a . b) to translate to a( . b)
-      (display (car obj))
-      (display "(")
-      (sweet-write-contents " " (cdr obj))
-      (display ")"))
+      (append
+        (modern-charlist (car obj))
+        (list LP)
+        (modern-contents '(#\space) (cdr obj))
+        (list RP)))
     (#t                  ; Generic list format.
-      (display "[")
-      (sweet-write-contents " " obj)
-      (display "]"))
-  )
-)
+      (append '(#\[) (modern-contents '(#\space) obj) '(#\])))))
 
-(define (sweet-write-testreport obj)
+
+(define (modern-write obj)
+  (display (list->string (modern-charlist obj))))
+
+(define (modern-write-testreport obj)
   (display "Writing ")
   (write obj)
   (newline)
   (display "  ")
-  (sweet-write obj)
+  ; (write (modern-charlist obj)) (newline)
+  (display (list->string (modern-charlist obj)))
   (newline)
   (newline))
 
-(sweet-write-testreport 'a)
-(sweet-write-testreport ''a)
-(sweet-write-testreport '())
-(sweet-write-testreport '(a))
-(sweet-write-testreport '(a b))
-(sweet-write-testreport '(a b c))
-(sweet-write-testreport '(a . b))
-(sweet-write-testreport '(a b . c))
-(sweet-write-testreport '(()))
-(sweet-write-testreport '(() a))
-(sweet-write-testreport '((x) a))
-(sweet-write-testreport '((x y) a))
-(sweet-write-testreport '((x y) (a b)))
-(sweet-write-testreport '(+))
-(sweet-write-testreport '(+ 3))
-(sweet-write-testreport '(+ 3 4))
-(sweet-write-testreport '(+ 3 4 5))
-(sweet-write-testreport '(f +))
-(sweet-write-testreport '(f (a b)))
-(sweet-write-testreport '(f (- n 1)))
-(sweet-write-testreport '(f (+ a b c)))
-(sweet-write-testreport '(+ 3 (+ 4 5)))
+(modern-write-testreport 'a)
+(modern-write-testreport ''a)
+(modern-write-testreport '())
+(modern-write-testreport '(a))
+(modern-write-testreport '(a b))
+(modern-write-testreport '(a b c))
+(modern-write-testreport '(a . b))
+(modern-write-testreport '(a b . c))
+(modern-write-testreport '(()))
+(modern-write-testreport '(() a))
+(modern-write-testreport '((x) a))
+(modern-write-testreport '((x y) a))
+(modern-write-testreport '((x y) (a b)))
+(modern-write-testreport '(+))
+(modern-write-testreport '(+ 3))
+(modern-write-testreport '(+ 3 4))
+(modern-write-testreport '(+ 3 4 5))
+(modern-write-testreport '(f +))
+(modern-write-testreport '(f (a b)))
+(modern-write-testreport '(f (- n 1)))
+(modern-write-testreport '(f (+ a b c)))
+(modern-write-testreport '(+ 3 (+ 4 5)))
 
 
