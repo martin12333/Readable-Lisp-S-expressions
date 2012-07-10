8169f7df76a58d91e178539957655d4b4cd3688c
Alan Manuel K. Gloria <almkglor@gmail.com>
2012-07-10 15:27:17 +0800

    Correct underlying-read in modern.scm, in a part that doesn't actually get called.

--- modern.scm
+++ modern.scm
@@ -272,8 +272,7 @@
                (underlying-read port)))
            (#t 
             (list 'unquote
-              (underlying-read input-stream eof-error-p
-                            eof-value recursive-p)))))
+              (underlying-read port)))))
       ; The "(" calls modern-read, but since this one shouldn't normally
       ; be used anyway (modern-read will get first crack at it), it
       ; doesn't matter:
