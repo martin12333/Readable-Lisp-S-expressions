98a3712f9199034eb4a35980973f4cde2eb28053
David A. Wheeler <dwheeler@dwheeler.com>
2013-01-24 18:09:07 -0500

    sweet.g: Move Lisp/Scheme basic procedures into sweet.g
      - This move makes it easier to see "everything in one place", simplifying
        installation and use by others.

--- output/Unsweeten_ANTLR.java
+++ output/Unsweeten_ANTLR.java
@@ -8,30 +8,14 @@ import static scheme.Pair.*;
 
 public class Unsweeten_ANTLR {
   public static void main(String[] args) throws Exception {
-    // System.out.print("Starting\n");
-    Object x = cons(1, cons(2, null));
-    Object y = list("a", "b");
-    Object z = append(x, y);
-    // System.out.print("pair x = " + string_datum(x) + "\n");
-    // System.out.print("pair y = " + string_datum(y) + "\n");
-    // System.out.print("pair z = " + string_datum(z) + "\n");
-    // System.out.print("DEBUG 1:\n");
     ANTLRInputStream input = new ANTLRInputStream(System.in);
-    // System.out.print("DEBUG 2:\n");
     sweetLexer lexer = new sweetLexer(input);
-    // System.out.print("DEBUG 3:\n");
     CommonTokenStream tokens = new CommonTokenStream(lexer);
 
-    // System.out.print("DEBUG 4:\n");
     sweetParser parser = new sweetParser(tokens);
-    // System.out.print("About to parse:\n");
-    // parser.t_expr();
     while (true) {
       parser.start();
     }
-    // System.out.print("\n");
-    // System.out.print("DEBUG 5\n");
-    // System.out.print("\n");
   }
 }
 
