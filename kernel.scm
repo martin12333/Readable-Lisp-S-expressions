e042b2e276ce93a0edb01e8aa7adb2de20d2dda8
David A. Wheeler <dwheeler@dwheeler.com>
2013-03-06 18:59:16 -0500

    SRFI-110.html: Repair URL for reference implementation
      - The SRFI site has a slightly different organization of code.
        So let's link to "kernel.scm", and add a stub file so that
        people who have a local copy will know where to go.

---
+++ kernel.scm
@@ -0,0 +1,9 @@
+<html>
+<head>
+<title>See src/kernel.scm</title>
+</head>
+<body>
+See <a href="src/kernel.scm">src/kernel.scm</a> for the
+reference implementation of sweet-expressions.
+</body>
+</html>
