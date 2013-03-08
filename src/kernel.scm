90ab5ad4255a0b062f069d73b39a837b141b0581
Alan Manuel K. Gloria <almkglor@gmail.com>
2013-03-08 22:04:40 +0800

    src/kernel.scm: Protect against peek-char consuming EOF in Guile.

--- src/kernel.scm
+++ src/kernel.scm
@@ -174,26 +174,47 @@
     ; getting confused on the distinction between compile-time,
     ; load-time and run-time (apparently, peek-char is not bound
     ; during load-time).
-    (define (my-peek-char p)     (peek-char p))
-    (define (my-read-char p)     (read-char p))
+    (define (my-peek-char fake-port)
+      (if (eof-object? (car fake-port))
+          (car fake-port)
+          (let* ((port (car fake-port))
+                 (char (peek-char port)))
+            (if (eof-object? char)
+              (set-car! fake-port char))
+            char)))
+    (define (my-read-char fake-port)
+      (if (eof-object? (car fake-port))
+          (car fake-port)
+          (let* ((port (car fake-port))
+                 (char (read-char port)))
+            (if (eof-object? char)
+              (set-car! fake-port char))
+            char)))
 
     (define (make-read f)
       (lambda args
         (let ((port (if (null? args) (current-input-port) (car args))))
-          (f port))))
+          (f (list port)))))
 
-    (define (invoke-read read port)
-      (read port))
+    (define (invoke-read read fake-port)
+      (if (eof-object? (car fake-port))
+        (car fake-port)
+        (read (car fake-port))))
 
     ; create a list with the source information
-    (define (get-sourceinfo port)
-      (list (port-filename port)
-            (port-line port)
-            (port-column port)))
+    (define (get-sourceinfo fake-port)
+      (if (eof-object? (car fake-port))
+          #f
+          (let ((port (car fake-port)))
+            (list (port-filename port)
+                  (port-line port)
+                  (port-column port)))))
     ; destruct the list and attach, but only to cons cells, since
     ; only that is reliably supported across Guile versions.
     (define (attach-sourceinfo pos obj)
       (cond
+        ((not pos)
+          obj)
         ((pair? obj)
           (set-source-property! obj 'filename (list-ref pos 0))
           (set-source-property! obj 'line     (list-ref pos 1))
