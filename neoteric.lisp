bb84b81714f5d6b0d572c4390cd033fdf0bdcdc4
David A. Wheeler <dwheeler@dwheeler.com>
2013-04-24 17:42:45 -0400

    neoteric.lisp: Clean up basic neoteric-expression support
      - Remove many in-line DEBUG statements, etc.

--- neoteric.lisp
+++ neoteric.lisp
@@ -80,8 +80,6 @@
     ; Do NOT make recursive, or spaces after atoms will be consumed.
     (let ((atom (read-preserving-whitespace stream t nil)))
       (setq *readtable* saved-readtable)
-      ; (princ "DEBUG atom=") (write atom) (terpri)
-      ; (princ "DEBUG peek-char=") (write (peek-char nil stream)) (terpri)
       (neoteric-process-tail stream atom))))
 
 (defun wrap-continue (stream char)
@@ -149,17 +147,14 @@
 ; Nonsense marker for eof
 (defconstant neoteric-eof-marker (cons 'eof '()))
 
-; TODO: Handle eof as directed by "read".  Not currently consistent.
-
 ; TODO: sbcl reports the following error:
 ; ; file: /home/dwheeler/readable-code/neoteric.lisp
 ; ; in: DEFUN READABLE::MY-READ-DELIMITED-LIST
 ; ;     (READABLE::READ-ERROR "Bad closing character")
 ; ; caught STYLE-WARNING:
 ; ;   undefined function: READABLE::READ-ERROR
-;
 
-; TODO
+; TODO: Handle eof as directed by "read".  Not currently consistent.
 (defun read-error (message)
   (declare (ignore message))
   nil)
@@ -171,11 +166,9 @@
     #\' #\` #\,))
 
 
+; TODO: Make "..." illegal, and maybe anything with just multiple ".".
 
-; TODO: Ensure "a(. b)" and "(. x)" correctly handled.
-; TODO: Must be able to handle ".", e.g., "a(. b)" and "a(b . c)".
-; Use format (concatenate 'string "hi" (string #\q))
-
+; Read a datum and ALLOW "." as a possible value:
 (defun my-read-to-delimiter (input-stream)
   (let*
     ((*readtable* *neoteric-underlying-readtable*) ; Temporary switch
@@ -184,36 +177,22 @@
         until (find (peek-char nil input-stream) neoteric-delimiters)
         collect (read-char input-stream)))
      (my-string (concatenate 'string clist)))
-    (princ "DEBUG: clist=") (write clist) (terpri) (princ "my-string=") (write my-string) (terpri)
-    ; (princ "DEBUG: read-from-string=") (write (read-from-string my-string))
     (if (string= my-string ".")
         '|.|
         (read-from-string my-string))))
 
 (defun my-read-datum (input-stream)
- (princ "DEBUG: entering my-read-datum") (terpri)
   (let* ((c (peek-char t input-stream))) ; Consume leading whitespace
     (cond
-      ((eql c #\.)
-        (princ "DEBUG: Found leading .") (terpri)
-        ; TODO: Read chars and determine what it is.
-        ; (read-preserving-whitespace input-stream t nil) ; TODO
-        ; For now, assume that leading "." is just "."
-        ; (read-char input-stream) ; Consume "."
-        ; (princ "DEBUG: Returning from finding leading .") (terpri)
-        ; '|.|
+      ((eql c #\.) ; Use specialized reader if starts with "."
         (my-read-to-delimiter input-stream))
       (t (read-preserving-whitespace input-stream t nil)))))
 
 
 (defun my-read-delimited-list (stop-char input-stream)
- (princ "DEBUG: entering my-read-deliminated-list") (terpri)
  (handler-case
   (let* ((c (peek-char t input-stream))) ; First consume leading whitespace
-    ; (princ "DEBUG enter my-read-delimited-list peek=") (write c) (terpri)
     (cond
-      ; TODO:
-      ; ((eof-object? c) (read-error "EOF in middle of list") '())
       ((eql c stop-char)
         (read-char input-stream)
         '())
@@ -224,13 +203,9 @@
       (t
         ; Must preserve whitespace so "a ()" isn't read as "a()"
         (let ((datum (my-read-datum input-stream)))
-          ; (princ "DEBUG enter my-read-delimited-list post-datum peek=")
-          ; (write (peek-char nil input-stream))
-          ; (princ " datum=") (write datum) (terpri)
           (cond
              ; Note: "." only counts as cdr-setting if it begins with "."
              ((and (eq datum '|.|) (eql c #\.))
-               (princ "DEBUG found '.' operator") (terpri)
                (let ((datum2 (read-preserving-whitespace input-stream t nil)))
                  ; (consume-whitespace input-stream)
                  (cond
@@ -271,7 +246,6 @@
                   (cons prefix
                     (my-read-delimited-list #\] input-stream)))))
         ((eql c #\{ )  ; Implement f{x}.
-          ; (princ "DEBUG {} tail") (terpri)
           (read-char input-stream nil nil t) ; consume opening char
           (neoteric-process-tail input-stream
             (let ((tail (process-curly
@@ -282,104 +256,7 @@
         (t prefix))))
 
 
-; OLD JUNK:
-; (Will delete once the new system is implemented.)
 
-;   ; Read, then process it through neoteric-tail to manage suffixes.
-;   ; Unlike Scheme, in Common Lisp we can force the reader to consider
-;   ; {} and [] as delimiters, so we don't have to re-implement some parts of
-;   ; the reader.  However, Common Lisp's approach to backquote and comma is
-;   ; more complicated to implement.
-;   (defun neoteric-read (&optional (input-stream *standard-input*)
-;                           (eof-error-p t) (eof-value nil) (recursive-p nil))
-;    (handler-case
-;     (neoteric-process-tail input-stream
-;       (let* ((c (peek-char t input-stream)))
-;         (cond
-;           ((eql c #\( )
-;             (read-char nil input-stream)
-;             (my-read-delimited-list #\) input-stream))
-;           ((eql c #\{ )
-;             (read-char nil input-stream)
-;             (process-curly
-;               (my-read-delimited-list #\} input-stream)))
-;           ((eql c #\; )
-;             (read-line input-stream) ; Consume rest of the line.
-;             (neoteric-read input-stream eof-error-p eof-value recursive-p))
-;           ((eql c #\' )
-;             (read-char nil input-stream)
-;             (list 'quote
-;               (neoteric-read input-stream eof-error-p eof-value recursive-p)))
-;           ; TODO: Add support for comma, backquote, etc.
-;           ; The Common Lisp hyperspec says in
-;           ; "2.4.6.1 Notes about Backquote":
-;           ; Since the exact manner in which the Lisp reader will parse an
-;           ; expression involving the backquote reader macro is not specified,
-;           ; an implementation is free to choose any representation that
-;           ; preserves the semantics described.
-;           ; Often an implementation will choose a representation that
-;           ; facilitates pretty printing of the expression, so that
-;           ; (pprint `(a ,b)) will display `(a ,b) and not, for example,
-;           ; (list 'a b). However, this is not a requirement.
-;           ; Implementors who have
-;           ; no particular reason to make one choice or another might wish
-;           ; to refer to IEEE Standard for the Scheme Programming Language,
-;           ; which identifies a popular choice of representation for such
-;           ; expressions that might provide useful to be useful compatibility
-;           ; for some user communities. There is no requirement, however,
-;           ; that any conforming implementation use this particular
-;           ; representation. This information is provided merely for
-;           ; cross-reference purposes.
-;           (t
-;             ; Force recursive; even at the top level, when it reads what
-;             ; it *thinks* is a whole expression, we may be "inside" a
-;             ; longer list.  Otherwise, EOF handling can mess up.
-;             (read input-stream eof-error-p eof-value t)))))
-;     (end-of-file (e)
-;       (if eof-error-p
-;         (signal e) ; re-raise error
-;         eof-value))))
-;   
-;   ; Remarkably, Common Lisp provides no standard way to exit an image.
-;   ; Here's a mostly-portable mechanism to do so, from:
-;   ; http://www.cliki.net/Portable%20Exit
-;   (defun my-portable-exit (&optional code)
-;         ;; This group from "clocc-port/ext.lisp"
-;         #+allegro (excl:exit code)
-;         #+clisp (#+lisp=cl ext:quit #-lisp=cl lisp:quit code)
-;         #+cmu (ext:quit code)
-;         #+cormanlisp (win32:exitprocess code)
-;         #+gcl (lisp:bye code)                     ; XXX Or is it LISP::QUIT?
-;         #+lispworks (lw:quit :status code)
-;         #+lucid (lcl:quit code)
-;         #+sbcl (sb-ext:quit
-;                 :unix-code (typecase code (number code) (null 0) (t 1)))
-;         ;; This group from Maxima
-;         #+kcl (lisp::bye)                         ; XXX Does this take an arg?
-;         #+scl (ext:quit code)                     ; XXX Pretty sure this *does*.
-;         #+(or openmcl mcl) (ccl::quit)
-;         #+abcl (cl-user::quit)
-;         #+ecl (si:quit)
-;         ;; This group from 
-;         #+poplog (poplog::bye)                    ; XXX Does this take an arg?
-;         #-(or allegro clisp cmu cormanlisp gcl lispworks lucid sbcl
-;               kcl scl openmcl mcl abcl ecl)
-;         (error 'not-implemented :proc (list 'quit code)))
-;   
-;   ; Common Lisp provides no way to fully override the "read" function
-;   ; such as (defun read #'neoteric-read).
-;   ; We partly simulate it here by writing our own REPL, and exiting Lisp
-;   ; when it ends.  The simulation is quite imperfect (a call to "read"
-;   ; reveals the facade), but it's a start.
-;   ; Perhaps we should special-case "load" here.
-;   (defun enable-neoteric ()
-;     (handler-case
-;       (do ((result (neoteric-read) (neoteric-read)))
-;         (nil nil)
-;         (write (eval result))
-;         (terpri))
-;       (end-of-file () (my-portable-exit 0))))
-;   
 ;   (defun neoteric-filter ()
 ;     (handler-case
 ;       (do ((result (neoteric-read) (neoteric-read)))
@@ -396,12 +273,6 @@
 ;         (eval result)))
 ;     (end-of-file () )))
 ;   
-;   ; TODO: Handle improper lists, e.g., {a + b . c} should insert $nfx$, and
-;   ; port(a . b) should generate (port a . b).
-;   
-;   ; TODO: Handle #|...|# comments inside neoteric-read.
-;   
-;   
 ;   ; Likely things to do from here:
 ;   
 ;   ; (enable-neoteric)
@@ -413,27 +284,7 @@
 ;   ;   (terpri))
 ;   
 ;   
-;   (defun enable-curly-infix ()
-;     ; The following install the {...} reader.
-;     ; See "Common Lisp: The Language" by Guy L. Steele, 2nd edition,
-;     ; pp. 542-548 and pp. 571-572.
-;     ; Invoke curly-brace-infix-reader when opening curly brace is read in:
-;     (set-macro-character #\{ #'curly-brace-infix-reader)
-;     ; This is necessary, else a cuddled closing brace will be part of an atom:
-;     ; Borrow end of (.
-;     (set-macro-character #\} (get-macro-character #\) nil))
-;     ; Make "[" and "]" delimiters. For now, make same as ().
-;     (set-macro-character #\[ (get-macro-character #\( nil))
-;     (set-macro-character #\] (get-macro-character #\) nil))
-;     nil)
-;   
-;   ; The following install the {...} reader.
-;   ; See "Common Lisp: The Language" by Guy L. Steele, 2nd edition,
-;   ; pp. 542-548 and pp. 571-572.
-;   
-;   ; Read until }, then process list as infix list.
-;   ; This shouldn't be invoked once we use neoteric-read; it takes precedence.
-;   (defun curly-brace-infix-reader (stream char)
-;     (let ((result (read-delimited-list #\} stream t)))
-;       (process-curly result)))
-;   
+
+; TODO: Support enabling full curly-infix without neoteric-expressions
+; outside {...}.
+
