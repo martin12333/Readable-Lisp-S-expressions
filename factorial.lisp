54ec836c62a47393d8d72096c3932a93830d2f20
David A. Wheeler <dwheeler@dwheeler.com>
2013-06-17 20:08:30 -0400

    factorial.lisp: Tweak, in particular, make sections even more obvious

--- factorial.lisp
+++ factorial.lisp
@@ -1,14 +1,14 @@
-; Trivial sweet-expression Common Lisp demo.  Run as:
+; This is a trivial demo of sweet-expressions in Common Lisp.  Run as:
 ;     clisp -q -q factorial.lisp
-
 ; To run the NOT installed readable version, prepend that with:
 ;     CL_SOURCE_REGISTRY="$PWD" 
 
 (require "asdf")
 (asdf:load-system :readable)
 (readable:enable-sweet)
+(setq *print-escape* nil) ; Optional
 
-(setq *print-escape* nil)
+
 
 defun fact (x)
   if {x < 1}
@@ -16,5 +16,7 @@ defun fact (x)
     {x * fact{x - 1}}
 
 princ fact(5)
+
+
 
 (readable:disable-readable)
